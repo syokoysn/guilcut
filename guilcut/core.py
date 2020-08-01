87d1deba409e1663e5af5a3d5125dc96080bdfc5
shokoysn <syokoysn@gmail.com>
2020-08-02 03:50:47 +0900

    add sequence_pair

--- guilcut/core.py
+++ guilcut/core.py
@@ -15,3 +15,20 @@ def make_sequence(I, df_item_data):
             if (flag[i] in I) & (flag[i+1]in I) :
                 sequence.append((flag[i],flag[i+1]))
     return  sequence
+
+
+def sequence_pair(L1,L2):
+    '''
+    相対位置の関係より，自分より右か，したかに配置する情報を取得する関数
+    '''
+    b = {j:i for i,j in enumerate(L2)}
+    res = {i:[[],[]] for i in L1}
+    for i in range(len(L1)) :
+        for j in range(i+1,len(L1)):
+            if b[L1[i]]<b[L1[j]] :
+                #右に配置
+                res[L1[i]][1].append(L1[j])
+            else :
+                #下に配置
+                res[L1[i]][0].append(L1[j])
+    return res       
