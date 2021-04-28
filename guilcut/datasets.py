9a8c8abd2458c7aa789d5961101b7947a1c5b762
syokoysn <syoko_ysn@outlook.jp>
2021-04-28 13:37:37 +0000

    create

---
+++ guilcut/datasets.py
@@ -0,0 +1,22 @@
+from os.path import dirname
+import pandas as pd
+
+
+def load_roadef2018(kind='A', number=0):
+
+    dir_name = dirname(__file__) + f'/data/roadef2018/dataset_{kind}/'
+
+    batch_path = dir_name + kind + str(number) + '_batch.csv'
+    defect_path = dir_name + kind + str(number) + '_defects.csv'
+    if number == 0:
+        if kind == 'A':
+            n = 20
+        else:
+            n = 15
+        result = {}
+        for i in range(1, n+1):
+            batch_path = dir_name + kind + str(i)+'_batch.csv'
+            defect_path = dir_name + kind + str(i)+'_defects.csv'
+            result[i] = {'batch': pd.read_csv(batch_path, sep=';'), 'defect': pd.read_csv(defect_path, sep=';')}
+        return result
+    return {'batch': pd.read_csv(batch_path, sep=';'), 'defect': pd.read_csv(defect_path, sep=';')}
